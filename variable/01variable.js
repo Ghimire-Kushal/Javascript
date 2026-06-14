06595e50581bdac6f563fd0594fd1b0795ac2868
Kushal Ghimire <kushal.upr@gmail.com>
2026-06-14 12:21:55 +0545

    create variable

---
+++ variable/01variable.js
@@ -0,0 +1,10 @@
+const accountId = 12345;
+let accountName = 'Kushal';
+var accountBalance = 1000;
+
+let accountState;
+// accountName = "Kushal Ghimire"
+// accountId = 121212
+
+console.log('Account ID:', accountId);
+console.table([ accountId,accountName, accountBalance, accountState] );
