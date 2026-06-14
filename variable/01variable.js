9088d8dbe104a8dfca45c0c56cc31a22770626c9
Kushal Ghimire <kushal.upr@gmail.com>
2026-06-14 12:24:43 +0545

    add value

--- variable/01variable.js
+++ variable/01variable.js
@@ -3,8 +3,12 @@ let accountName = 'Kushal';
 var accountBalance = 1000;
 
 let accountState;
+accountName = 'Kushal Ghimire';
+accountBalance = 2000;
+accountState = 'Active';
+
 // accountName = "Kushal Ghimire"
-// accountId = 121212
+// accountId = 12121å2
 
 console.log('Account ID:', accountId);
 console.table([ accountId,accountName, accountBalance, accountState] );
